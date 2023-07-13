bdd646f769d38ca946567fff0b6afdbbfb08a21a
BehnamGolmohammadi <gt.bg.78@gmail.com>
2023-07-13 16:21:53 +0330

    A short edit because of error.

--- CW15/BlogPlatform/BlogPlatform/urls.py
+++ CW15/BlogPlatform/BlogPlatform/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog', include("BlogApp.urls", namespace= "blog")),
+    path('blog', include("BlogApp.urls")),
 ]
 
 
