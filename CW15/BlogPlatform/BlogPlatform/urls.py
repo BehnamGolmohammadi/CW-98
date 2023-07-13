3e15b8d9da4298f66d941cdc7a82d09e40dd9fb4
BehnamGolmohammadi <gt.bg.78@gmail.com>
2023-07-13 16:28:39 +0330

    git change urls format.

--- CW15/BlogPlatform/BlogPlatform/urls.py
+++ CW15/BlogPlatform/BlogPlatform/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog', include("BlogApp.urls")),
+    path('blog/', include("BlogApp.urls")),
 ]
 
 
