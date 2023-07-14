37e4a7c12efa1f01e3f751e85b4c5c09c59154b8
BehnamGolmohammadi <gt.bg.78@gmail.com>
2023-07-14 14:17:54 +0330

    Some edit done and a field added.

--- CW15/BlogPlatform/PostApp/models.py
+++ CW15/BlogPlatform/PostApp/models.py
@@ -6,12 +6,13 @@ class Post(models.Model):
     title = models.CharField(max_length=64)
     subject = models.CharField(max_length=128)
     content = models.TextField()
-    publish_date = models.DateField(auto_now_add= True)
+    created_date = models.DateField(auto_now_add=True)
+    publish_date = models.DateField(null= True)
     update_date = models.DateField(auto_now=True)
 
 
     def __str__(self) -> str:
-        text = f"Title: {self.name}\nSubject: {self.subject}\nContent: {self.bio}\Created at: {self.Creation_date}\n"
+        text = f"Title: {self.title}\nSubject: {self.subject}\nContent: {self.content}\Created at: {self.publish_date}\n"
         return text
 
 
