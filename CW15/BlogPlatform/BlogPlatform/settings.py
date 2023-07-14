0bf5ec89325fc893ca11a0255ceae1e581b743b3
BehnamGolmohammadi <gt.bg.78@gmail.com>
2023-07-14 10:47:24 +0330

    Install new apps.

--- CW15/BlogPlatform/BlogPlatform/settings.py
+++ CW15/BlogPlatform/BlogPlatform/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'BlogApp.apps.BlogappConfig',
+    'AuthorApp.apps.AuthorappConfig',
+    'PostApp.apps.PostappConfig',
+    'CategoryApp.apps.CategoryappConfig',
 ]
 
 MIDDLEWARE = [
