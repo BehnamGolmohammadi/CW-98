70f8a7615847feba5980be8249a6c8b6e06c02ec
BehnamGolmohammadi <gt.bg.78@gmail.com>
2023-07-14 14:57:42 +0330

    Add image field to Author model.

--- CW15/BlogPlatform/AuthorApp/models.py
+++ CW15/BlogPlatform/AuthorApp/models.py
@@ -1,9 +1,13 @@
 from django.db import models
 
 # Create your models here.
+def user_img_directory(instance, filename):
+    return "Author_image/user_%s/%s" %(instance, filename)
+
 class Author(models.Model):
     name = models.CharField(max_length=32)
     bio = models.CharField(max_length=256)
+    image = models.ImageField(upload_to=user_img_directory)
     register_date = models.DateField(auto_now_add= True)
 
     def __str__(self) -> str:
