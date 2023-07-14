6ae754434bdc3b7b667a7edce4b346f0823475fe
BehnamGolmohammadi <gt.bg.78@gmail.com>
2023-07-14 15:12:25 +0330

    change username of db

--- CW15/BlogPlatform/BlogPlatform/settings.py
+++ CW15/BlogPlatform/BlogPlatform/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'maktab_dj', 
-        'USER': 'Behnam', 
+        'USER': 'postgres', 
         'PASSWORD': 'Behnam.2201',
         'HOST': '127.0.0.1', 
         'PORT': '5432',
