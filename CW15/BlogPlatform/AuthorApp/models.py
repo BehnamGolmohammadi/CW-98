831bc6329606a550d1ef2892731ce8a5b707be9d
BehnamGolmohammadi <gt.bg.78@gmail.com>
2023-07-14 11:07:11 +0330

    Create Author model.

--- CW15/BlogPlatform/AuthorApp/models.py
+++ CW15/BlogPlatform/AuthorApp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=32)
+    bio = models.CharField(max_length=256)
+    register_date = models.DateField(auto_now_add= True)
+
+    def __str__(self) -> str:
+        text = f"Name: {self.name}\nBiography: {self.bio}\Registerd at: {self.register_date}\n"
+        return text
