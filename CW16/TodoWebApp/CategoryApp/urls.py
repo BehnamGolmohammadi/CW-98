643fec4a74f20c901bbeb13e82acc1ac9837dad9
BehnamGolmohammadi <gt.bg.78@gmail.com>
2023-07-20 16:41:06 +0330

    feat: create a url and route to view to all tasks

--- CW16/TodoWebApp/CategoryApp/urls.py
+++ CW16/TodoWebApp/CategoryApp/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'category'
 
 urlpatterns = [
-    
+    path('', views.all_category ,name= 'index'),
+    # path('category/<int:task_id>', views.task_detail, name='categoryinfo')
 ]
--- CW16/TodoWebApp/CategoryApp/views.py
+++ CW16/TodoWebApp/CategoryApp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from TaskApp.models import Task, Category
+
 
 # Create your views here.
+def all_category(request):
+    all_category = Category.objects.all()
+    return render(request, 'category/category.html', context= {'all_category': all_category})
