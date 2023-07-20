89d569c176b0a9b7ad7dbb5330a05cddc92f3d49
BehnamGolmohammadi <gt.bg.78@gmail.com>
2023-07-20 14:54:34 +0330

    feat:create Category model.

--- CW16/TodoWebApp/CategoryApp/models.py
+++ CW16/TodoWebApp/CategoryApp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Category:
+    name = models.CharField(max_length=128)
+    description = models.TextField()
+
+    def __str__(self) -> str:
+        return f"{self.name}"
