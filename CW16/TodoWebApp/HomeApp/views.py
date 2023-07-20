af99e0c5012c29fc04478aff1077b3365ecd6020
BehnamGolmohammadi <gt.bg.78@gmail.com>
2023-07-20 16:48:07 +0330

    chore: show category in task list in home page.

--- CW16/TodoWebApp/HomeApp/views.py
+++ CW16/TodoWebApp/HomeApp/views.py
@@ -3,6 +3,6 @@ from TaskApp.models import Task, Category
 
 # Create your views here.
 def home_page(request):
-    all_task = Task.objects.all().values()[:5]
+    all_task = Task.objects.all()[:5]
     all_category = Category.objects.all()[:5]
     return render(request, 'home/home.html', {'all_task':all_task, 'all_category':all_category})
