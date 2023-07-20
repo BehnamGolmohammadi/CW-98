5a91404dc406cc7aed910eab7126404294809645
BehnamGolmohammadi <gt.bg.78@gmail.com>
2023-07-20 14:49:42 +0330

    chore:append category to install app.

--- CW16/TodoWebApp/TodoWebApp/settings.py
+++ CW16/TodoWebApp/TodoWebApp/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'HomeApp.apps.HomeappConfig',
     'TaskApp.apps.TaskappConfig',
+    'CategoryApp.apps.CategoryappConfig',
 ]
 
 MIDDLEWARE = [
