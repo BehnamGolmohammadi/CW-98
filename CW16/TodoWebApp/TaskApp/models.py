bbc3845f91008f934c34d4cb506364f44398d498
BehnamGolmohammadi <gt.bg.78@gmail.com>
2023-07-20 15:06:57 +0330

    feat:Create new field for Task model

--- CW16/TodoWebApp/TaskApp/models.py
+++ CW16/TodoWebApp/TaskApp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from CategoryApp.models import Category
 
 # Create your models here.
 class Tag(models.Model):
@@ -16,6 +17,7 @@ class Task(models.Model):
     status_choices = (('Todo','Todo'), ('Done','Done'), ('Doing','Doing'))
     status = models.CharField(max_length=5, choices=status_choices)
     tag = models.ManyToManyField(Tag)
+    category = models.ManyToManyField(Category)
 
     def __str__(self) -> str:
         return f"{self.title} | {self.due_date} | {self.status}"
