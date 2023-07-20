13b3a97d269b765f33239d7a1abb57cf24afcfd4
BehnamGolmohammadi <gt.bg.78@gmail.com>
2023-07-20 14:56:14 +0330

    fix:add inheritance.

--- CW16/TodoWebApp/CategoryApp/models.py
+++ CW16/TodoWebApp/CategoryApp/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Category:
+class Category(models.Model):
     name = models.CharField(max_length=128)
     description = models.TextField()
 
