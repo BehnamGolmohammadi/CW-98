0d23779a35f5b072c6684ff9c5a9fecf24cc84d6
BehnamGolmohammadi <gt.bg.78@gmail.com>
2023-07-21 06:33:17 +0330

    feat:Insert pagination.

--- CW16/TodoWebApp/TaskApp/views.py
+++ CW16/TodoWebApp/TaskApp/views.py
@@ -1,10 +1,24 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from .models import Task
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 # Create your views here.
 def all_task(request):
     all_task = Task.objects.all()
-    return render(request, 'task/task.html', context= {'all_task': all_task})
+    pg = Paginator(all_task, 5)
+    page_number = request.GET.get('page')
+    try:
+          page_obj = pg.get_page(page_number)
+    except PageNotAnInteger:
+          page_obj = pg.page(1)
+    except EmptyPage:
+          page_obj = pg.page(pg.num_pages)
+
+    
+          
+
+    return render(request, 'task/task.html', context= {'page_obj': page_obj})
+
 
 def task_detail(request, task_id):
        task = Task.objects.get(id=task_id)
