78a79a58424823e5ade4b175b409b4289fdd0565
BehnamGolmohammadi <gt.bg.78@gmail.com>
2023-08-03 16:45:24 +0330

    feat: Add author field to Task model.

--- CW18/TodoWebApp/TaskApp/models.py
+++ CW18/TodoWebApp/TaskApp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from CategoryApp.models import Category
+from UserApp.models import User
 
 # Create your models here.
 class Tag(models.Model):
@@ -18,6 +19,7 @@ class Task(models.Model):
     status = models.CharField(max_length=5, choices=status_choices)
     tag = models.ManyToManyField(Tag)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.PROTECT)
 
     def __str__(self) -> str:
         return f"{self.title} | {self.due_date} | {self.status}"
