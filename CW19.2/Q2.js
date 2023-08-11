267aabd9ecb65f73fc4f178afadb2d2182a0563a
BehnamGolmohammadi <gt.bg.78@gmail.com>
2023-08-11 15:17:08 +0330

    Question 2 done

---
+++ CW19.2/Q2.js
@@ -0,0 +1,14 @@
+let age = prompt("Enter your age:")
+
+if (age < 10){
+    console.log('child')
+}
+else if(age < 18){
+    console.log('teenager')
+}
+else if(age < 30){
+    console.log('young')
+}
+else{
+    console.log('adult')
+}
