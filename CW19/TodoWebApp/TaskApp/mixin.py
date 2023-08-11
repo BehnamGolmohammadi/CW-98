b15f9299da71088f17f3b757f19c0ad9b04615ce
BehnamGolmohammadi <gt.bg.78@gmail.com>
2023-08-11 10:27:38 +0330

    fix: Logic error fixed

--- CW19/TodoWebApp/TaskApp/mixin.py
+++ CW19/TodoWebApp/TaskApp/mixin.py
@@ -4,7 +4,7 @@ from django.core.exceptions import PermissionDenied as PD
 class TodoOwnerRequiredMixin:
     def dispatch(self, request, *args, **kwargs):
         task = Task.objects.get(id = kwargs['task_id'])
-        if not task.author == request.user:
+        if task.author == request.user:
             raise PD
         else:
             return super().dispatch(request, *args, **kwargs)
