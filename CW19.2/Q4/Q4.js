79d456018316e23cdf7fe50a48c3863d435ba9b8
BehnamGolmohammadi <gt.bg.78@gmail.com>
2023-08-11 16:54:56 +0330

    Question4 Done

---
+++ CW19.2/Q4/Q4.js
@@ -0,0 +1,9 @@
+// Get the button and DOM element
+const button = document.getElementById('myButton');
+const textElement = document.getElementById('myText');
+
+// Add event listener to the button
+button.addEventListener('click', function() {
+  // Change the text content of the DOM element
+  alert('Button clicked!');
+});
