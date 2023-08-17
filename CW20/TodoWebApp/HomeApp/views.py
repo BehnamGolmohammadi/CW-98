21c6a7fe63ab50c411cb5a86908fbaa98f776b63
BehnamGolmohammadi <gt.bg.78@gmail.com>
2023-08-17 15:05:15 +0330

    feat: Import List view and create subclass of it.

--- CW20/TodoWebApp/HomeApp/views.py
+++ CW20/TodoWebApp/HomeApp/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from TaskApp.models import Task, Category
-
+from django.views.generic import ListView
 # Create your views here.
-def home_page(request):
-    all_task = Task.objects.all()[:5]
-    all_category = Category.objects.all()[:5]
-    return render(request, 'home/home.html', {'all_task':all_task, 'all_category':all_category})
+# def home_page(request):
+#     all_task = Task.objects.all()[:5]
+#     all_category = Category.objects.all()[:5]
+#     return render(request, 'home/home.html', {'all_task':all_task, 'all_category':all_category})
+
+class HomePageView(ListView):
+    model = Task
+    template_name = 'home/home.html'
+    context_object_name = 'all_task'
+    queryset = Task.objects.all()[:5]
