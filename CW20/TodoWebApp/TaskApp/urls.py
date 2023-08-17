01d6574d553530d44c9ca4c31205c5f39e425972
BehnamGolmohammadi <gt.bg.78@gmail.com>
2023-08-17 16:52:22 +0330

    chore: change view in path of view

--- CW20/TodoWebApp/TaskApp/urls.py
+++ CW20/TodoWebApp/TaskApp/urls.py
@@ -7,7 +7,8 @@ urlpatterns = [
     path('', views.AllTask.as_view() ,name= 'index'),
     # path('taskinfo/<int:task_id>', views.TaskDetail.as_view(), name='taskinfo'),
     path('taskinfo/<int:pk>', views.TaskDetailView.as_view(), name='taskinfo'),
-    path('newtask/', views.NewTask.as_view(), name='newtask'),
+    # path('newtask/', views.NewTask.as_view(), name='newtask'),
+    path('newtask/', views.NewTaskView.as_view(), name='newtask'),
     path('updatetask/<int:task_id>', views.UpdateTask.as_view(), name='updatetask'),
     path('deletetask/<int:task_id>', views.DeleteTask.as_view(), name='deletetask'),
 ]
